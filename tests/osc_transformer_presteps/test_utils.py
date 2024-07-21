8252bc7d3dddc193f54f0d9f5311eae9396caadd
„veiy82l“ <david.besslich@investmentdataservices.com>
2024-07-21 22:01:56 +0200

    Make ruff pass after new version
    Signed-off-by: „veiy82l“ <david.besslich@investmentdataservices.com>

--- tests/osc_transformer_presteps/test_utils.py
+++ tests/osc_transformer_presteps/test_utils.py
@@ -1,3 +1,5 @@
+"""Module to test the utils.py."""
+
 import pytest
 
 from osc_transformer_presteps.utils import specify_root_logger, set_log_folder
@@ -11,24 +13,28 @@ def test_specify_root_logger():
     """Test the specify_root_logger function."""
     specify_root_logger(log_level=20, logs_folder=cwd)
     for file in cwd.iterdir():
-        if file.suffix == '.log':
+        if file.suffix == ".log":
             file.unlink()
     assert len(logging.root.handlers) == 2
-    assert type(logging.root.handlers[0]) == logging.StreamHandler
-    assert type(logging.root.handlers[1]) == logging.FileHandler
+    assert isinstance(logging.root.handlers[0], logging.StreamHandler)
+    assert isinstance(logging.root.handlers[1], logging.FileHandler)
     assert logging.root.handlers[0].level == 20
 
 
 class TestSetLogFolder:
+    """Class to collect tests for set_log_folder."""
 
     def test_set_log_folder_no_folder(self):
+        """Test set_log_folder without a given folder."""
         log_folder = set_log_folder(cwd=Path.cwd(), logs_folder=None)
         assert log_folder == Path.cwd()
 
     def test_set_log_folder_given_folder_exists(self):
-        log_folder = set_log_folder(cwd=Path.cwd(), logs_folder='data')
-        assert log_folder.name == 'data'
+        """Test set_log_folder with a given existing folder."""
+        log_folder = set_log_folder(cwd=Path.cwd(), logs_folder="data")
+        assert log_folder.name == "data"
 
     def test_set_log_folder_given_folder_not_existing(self):
+        """Test set_log_folder with a given non-existing folder."""
         with pytest.raises(AssertionError):
-            set_log_folder(cwd=Path.cwd(), logs_folder='bla')
+            set_log_folder(cwd=Path.cwd(), logs_folder="bla")

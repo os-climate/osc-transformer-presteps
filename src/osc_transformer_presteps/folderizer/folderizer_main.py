af3fc07c28e927bbfc442c46907231aeb8767b13
tanishq-ids <tanishq.more@incestmentdataservices.com>
2025-02-05 12:00:43 +0100

    added folderrizer command in cli
    Signed-off-by: tanishq-ids <tanishq.more@incestmentdataservices.com>

---
+++ src/osc_transformer_presteps/folderizer/folderizer_main.py
@@ -0,0 +1,40 @@
+"""Folderizer-MAIN file."""
+
+import os
+
+def create_osc_folder_structure(base_path: str):
+    """
+    Creates a predefined folder structure inside an 'OSC' directory at the given base path.
+
+    Parameters:
+    base_path (str): The base directory where the 'OSC' folder will be created.
+
+    The function creates the following subdirectories inside 'OSC':
+    - inputs/pdfs_training
+    - inputs/pdfs_inference
+    - logs
+    - outputs/jsons_training
+    - outputs/jsons_inference
+    - outputs/curated_data_rel
+    - outputs/curated_data_kpi
+    - model
+    - outputs/inference_rel
+    """
+    osc_path = os.path.join(base_path, "OSC")
+    folders = [
+        "inputs/pdfs_training",
+        "inputs/pdfs_inference",
+        "logs",
+        "outputs/jsons_training",
+        "outputs/jsons_inference",
+        "outputs/curated_data_rel",
+        "outputs/curated_data_kpi",
+        "model",
+        "outputs/inference_rel",
+    ]
+    
+    for folder in folders:
+        os.makedirs(os.path.join(osc_path, folder), exist_ok=True)
+    
+    print(f"Folder structure created successfully under: {osc_path}")
+

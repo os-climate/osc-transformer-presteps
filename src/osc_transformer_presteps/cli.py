3a708ab258c971cee76ddec5da0aee372b6dab4e
tanishq-ids <166009643+tanishq-ids@users.noreply.github.com>
2025-02-24 15:24:14 +0100

    added folderizer (#265)
    * changes in kpi_curator_main.py
    
    Signed-off-by: tanishq-ids <tanishq.more@incestmentdataservices.com>
    
    * added folderrizer command in cli
    
    Signed-off-by: tanishq-ids <tanishq.more@incestmentdataservices.com>
    
    * added more folders to folderrizer
    
    Signed-off-by: tanishq-ids <tanishq.more@incestmentdataservices.com>
    
    * Chore: pre-commit autoupdate
    
    * precommit errors solved
    
    Signed-off-by: tanishq-ids <tanishq.more@incestmentdataservices.com>
    
    * Chore: pre-commit autoupdate
    
    ---------
    
    Signed-off-by: tanishq-ids <tanishq.more@incestmentdataservices.com>
    Co-authored-by: tanishq-ids <tanishq.more@incestmentdataservices.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/osc_transformer_presteps/cli.py
+++ src/osc_transformer_presteps/cli.py
@@ -8,6 +8,7 @@ import typer
 from osc_transformer_presteps.run_local_extraction import app as extraction
 from osc_transformer_presteps.run_local_relevance_curation import app as curation
 from osc_transformer_presteps.run_local_kpi_curation import kpi_curator_app
+from osc_transformer_presteps.run_local_folder_structure_maker import folderizer_app
 
 # Define command structure with typer module
 app = typer.Typer(no_args_is_help=True)
@@ -30,6 +31,12 @@ app.add_typer(
 app.add_typer(
     kpi_curator_app,
     name="kpi-curation",
+    help="If you want to run local creation of dataset for kpi-detection task, then this is the subcommand to use.",
+)
+
+app.add_typer(
+    folderizer_app,
+    name="make-folder-structure",
     help="If you want to run local creation of dataset for kpi-detection task, then this is the subcommand to use.",
 )
 
